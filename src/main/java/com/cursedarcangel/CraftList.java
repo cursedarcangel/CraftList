6bcda11e9edd24d4410734f41b329c2d59d1e38b
cursedarcangel <71993617+cursedarcangel@users.noreply.github.com>
2020-11-22 19:02:10 -0500

    Finishing

--- src/main/java/com/cursedarcangel/CraftList.java
+++ src/main/java/com/cursedarcangel/CraftList.java
@@ -5,10 +5,7 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
-import java.util.Properties;
-import com.google.gson.JsonArray;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import java.util.Scanner;
 import java.util.HashMap;
 
@@ -24,22 +21,25 @@ public class CraftList {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Map<String, Integer> recipes = gson.fromJson(reader, Map.class);
-        HashMap<String, Object> materialsPerItem = new HashMap<String, Object>();
-        for (Map.Entry<?, ?> index : recipes.entrySet()) {
+        Map<String, Object > recipes = gson.fromJson(reader, Map.class);
+        HashMap<String, Object> materialsPerItem = new HashMap<>();
+        for (Map.Entry<String, Object > index : recipes.entrySet()) {
             materialsPerItem.put(String.valueOf(index.getKey()), index.getValue());
         }
 
         System.out.println("What do you want to craft?");
         String craft = scanner.nextLine();
-        Object mats = materialsPerItem.get(craft);
+        Map<String, Double> mats = (Map<String, Double>) materialsPerItem.get(craft);
+        int counter = 0;
 
         if (mats == null) {
             System.out.println("Sorry, we don't currently have that recipe. Try again! (No capital letters, no plural)");
         } else {
             System.out.println("How many do you want to craft?");
             int amount = scanner.nextInt();
-            System.out.println(String.valueOf(mats));
+            for (Map.Entry<String, Double> index : mats.entrySet()) {
+                System.out.println(index.getKey() + ":" + Math.round(index.getValue() * amount));
+            }
             //cleaning up
             try {
                 reader.close();
